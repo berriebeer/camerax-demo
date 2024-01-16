ba550d71e5e33140e518bef75d93812222c1b767
abelgerrits <abelgerrits@hotmail.com>
2024-01-16 23:22:48 +0100

    Add OpenGallery button on HomePage to select image. Uri received in onActivityresult method

--- camerax-demo/app/src/main/java/com/robertlevonyan/demo/camerax/HomePage.kt
+++ camerax-demo/app/src/main/java/com/robertlevonyan/demo/camerax/HomePage.kt
@@ -4,17 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import android.net.Uri
 
 
 class HomePage : AppCompatActivity() {
+
+    companion object {
+        const val PICK_PHOTO_REQUEST_CODE = 100 // This can be any integer you choose.
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-        val button: Button = findViewById(R.id.btnOpenCamera)
-        button.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        val buttonCamera: Button = findViewById(R.id.btnOpenCamera)
+        buttonCamera.setOnClickListener {
+            val intentCamera = Intent(this, MainActivity::class.java)
+            startActivity(intentCamera)
+        }
+
+        val buttonGallery: Button = findViewById(R.id.btnOpenGallery)
+        buttonGallery.setOnClickListener {
+            val intentGallery = Intent(Intent.ACTION_PICK)
+            intentGallery.type = "image/*"
+            startActivityForResult(intentGallery, PICK_PHOTO_REQUEST_CODE)
+
+        }
+}
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_PHOTO_REQUEST_CODE && resultCode == RESULT_OK) {
+            val selectedImage: Uri? = data?.data
+            // Use the selected image URI here
         }
     }
 }
