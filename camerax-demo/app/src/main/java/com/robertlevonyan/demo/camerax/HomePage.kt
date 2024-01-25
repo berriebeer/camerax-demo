31df59ec9ea843c208caa1bbc06c0fb2bef0cd68
abelgerrits <abelgerrits@hotmail.com>
2024-01-25 19:09:31 +0100

    Implement image selection in HomePage same way as in CameraFragment

--- camerax-demo/app/src/main/java/com/robertlevonyan/demo/camerax/HomePage.kt
+++ camerax-demo/app/src/main/java/com/robertlevonyan/demo/camerax/HomePage.kt
@@ -1,20 +1,29 @@
 package com.robertlevonyan.demo.camerax
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
 import android.content.Intent
 import android.net.Uri
-
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 
 class HomePage : AppCompatActivity() {
 
-    companion object {
-        const val PICK_PHOTO_REQUEST_CODE = 100 // This can be any integer you choose.
-    }
+    private lateinit var imagePickerLauncher: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        // Initialize the launcher
+        imagePickerLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            uri?.let {
+                val intentOpenCameraWithImage = Intent(this, MainActivity::class.java)
+                intentOpenCameraWithImage.putExtra("SelectedImageUri", it.toString())
+                startActivity(intentOpenCameraWithImage)
+            }
+        }
 
         val buttonCamera: Button = findViewById(R.id.btnOpenCamera)
         buttonCamera.setOnClickListener {
@@ -24,19 +33,8 @@ class HomePage : AppCompatActivity() {
 
         val buttonGallery: Button = findViewById(R.id.btnOpenGallery)
         buttonGallery.setOnClickListener {
-            val intentGallery = Intent(Intent.ACTION_PICK)
-            intentGallery.type = "image/*"
-            startActivityForResult(intentGallery, PICK_PHOTO_REQUEST_CODE)
-
-        }
-}
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == PICK_PHOTO_REQUEST_CODE && resultCode == RESULT_OK) {
-            val selectedImageUri: Uri? = data?.data
-            val intentOpenCameraWithImage = Intent(this, MainActivity::class.java)
-            intentOpenCameraWithImage.putExtra("SelectedImageUri", selectedImageUri.toString())
-            startActivity(intentOpenCameraWithImage)
+            // Use the launcher to start the activity for result
+            imagePickerLauncher.launch("image/*")
         }
     }
 }
